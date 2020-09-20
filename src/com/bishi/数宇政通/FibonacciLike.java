eadabc148aa131d98ab45b012cde36f4c887785d
cuiweiman <daocaoren22222@163.com>
2020-09-20 20:42:40 +0800

    '数宇政通'

---
+++ src/com/bishi/数宇政通/FibonacciLike.java
@@ -0,0 +1,62 @@
+package com.bishi.数宇政通;
+
+/**
+ * <p>
+ * 有一对小兔子，从出生后第2个月起，每个月都生一对兔子。
+ * 小兔子长到第2个月后，每个月又生一对兔子。
+ * 按此规律，假设没有兔子死亡，第一个月有一对刚出生的小兔子，问第n个月有多少对兔子？
+ * 分析：比斐波那契的兔子繁殖能力强，少一个月，从第一个月开始，得到的兔子队列是：1  2  3  5  8  13 21 34
+ * </p>
+ *
+ * @description: 斐波那契变形
+ * @date: 2020/9/20 20:34
+ * @author: wei·man cui
+ */
+public class FibonacciLike {
+    public static void main(String[] args) {
+        int item = 7;
+        int result = fibonacci(item);
+        int result2 = fibonacci2(item);
+        System.out.println(result + " " + result2);
+    }
+
+    /**
+     * 递归
+     *
+     * @param item 输入
+     * @return 输出
+     */
+    public static int fibonacci(int item) {
+        if (item == 1 || item == 2 || item == 3) {
+            return item;
+        } else if (item > 3) {
+            return fibonacci(item - 1) + fibonacci(item - 2);
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 非递归
+     *
+     * @param item 输入
+     * @return 输出
+     */
+    public static int fibonacci2(int item) {
+        if (item == 1 || item == 2 || item == 3) {
+            return 1;
+        } else if (item > 3) {
+            int f1 = 1;
+            int f2 = 2;
+            int result = 0;
+            for (int i = 0; i <= item - 3; i++) {
+                result = f1 + f2;
+                f1 = f2;
+                f2 = result;
+            }
+            return result;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/com/bishi/数宇政通/MinLoopSubString.java
@@ -0,0 +1,70 @@
+package com.bishi.数宇政通;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>
+ * 输入一个字符串，判断是否完全循环，若是则输出最小循环子串，否则输出自身。
+ * 输入：abababab，完全循环子串有：abab，ab，最短的是 ab，因此输出ab；
+ * 输入：abcabcab，没有完全循环子串，输出自身abcabcab。
+ * </p>
+ *
+ * @description: 最小循环子串
+ * @date: 2020/9/20 20:09
+ * @author: wei·man cui
+ */
+public class MinLoopSubString {
+
+    /**
+     * 判断 重复
+     *
+     * @param in 输入
+     * @return 输出
+     */
+    public static Set<String> getMinLoop(String in) {
+        Set<String> set = new HashSet<>();
+        if (in == null || in.length() <= 0) {
+            return set;
+        } else if (in.length() <= 3) {
+            set.add(in);
+            return set;
+        } else {
+            for (int i = 0; i < in.length(); i++) {
+                for (int j = i + 1; j < in.length(); j++) {
+                    // 循环获取子串
+                    String t = in.substring(i, j);
+                    if (in.replace(t, "").length() == 0) {
+                        // 放入集合中
+                        set.add(t);
+                    }
+                }
+            }
+            return set;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String in = "abcabcabcabcabc";
+        List<String> list = new ArrayList<>(getMinLoop(in));
+
+        if (list.size() == 0) {
+            // 不是一个循环串，输出自身
+            System.out.println(in);
+        } else {
+            // 有循环串，找出最小循环串
+            int index = 0;
+            int min = list.get(0).length();
+            for (int i = 0; i < list.size(); i++) {
+                if (min > list.get(0).length()) {
+                    min = list.get(0).length();
+                    index = i;
+                }
+            }
+            System.out.println(list.get(index));
+        }
+    }
+}
